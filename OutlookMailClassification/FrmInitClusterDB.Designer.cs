af79f1a99d830a7cd3f4f87a88b66f030ab1bfaa
Merlin Köhler <merlin-koehler@outlook.com>
2021-05-16 14:31:46 +0200

    [ADD] Configuration for initial clustering folder.

---
+++ OutlookMailClassification/FrmInitClusterDB.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace OutlookMailClassification
+{
+    partial class FrmInitClusterDB
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbDirectories = new System.Windows.Forms.ComboBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(249, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Please select the base directory used for clustering:";
+            // 
+            // cbDirectories
+            // 
+            this.cbDirectories.FormattingEnabled = true;
+            this.cbDirectories.Location = new System.Drawing.Point(16, 29);
+            this.cbDirectories.Name = "cbDirectories";
+            this.cbDirectories.Size = new System.Drawing.Size(545, 21);
+            this.cbDirectories.TabIndex = 1;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(486, 56);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(405, 56);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 3;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // FrmInitClusterDB
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(573, 83);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.cbDirectories);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmInitClusterDB";
+            this.Text = "FrmInitClusterDB";
+            this.Load += new System.EventHandler(this.FrmInitClusterDB_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbDirectories;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
---
+++ OutlookMailClassification/FrmInitClusterDB.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Outlook = Microsoft.Office.Interop.Outlook;
+
+namespace OutlookMailClassification
+{
+    public partial class FrmInitClusterDB : Form
+    {
+
+        public Outlook.Folder ClusterFolder;
+        private List<Outlook.Folder> Folders;
+        public FrmInitClusterDB(List<Outlook.Folder> folders)
+        {
+
+            InitializeComponent();
+
+            Folders = folders;
+            ClusterFolder = null;
+            foreach (Outlook.Folder folder in folders)
+            {
+                cbDirectories.Items.Add(folder.FolderPath);
+            }
+            cbDirectories.SelectedIndex = 1;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            ClusterFolder = Folders[cbDirectories.SelectedIndex];
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void FrmInitClusterDB_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
